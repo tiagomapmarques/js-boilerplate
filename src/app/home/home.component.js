658a436b2015d39e1f41550c4c9f17e707ca9afe
Tiago Marques <tiago.map.marques@gmail.com>
2017-11-04 20:47:18 +0000

    Updated app code

--- src/app/home/home.component.js
+++ src/app/home/home.component.js
@@ -1,20 +1,37 @@
+import { Logger } from './logger';
 
 export class HomeComponent {
+  static DATA_URL = '/assets/sample.json';
+  static FETCH_RESPONSE_ERROR = 'Fetch response not ok';
+  appId;
+
   constructor(id) {
     this.appId = id;
-    this.build = this.build.bind(this);
+    this.buildPage = this.buildPage.bind(this);
   }
 
   run() {
-    this.getSampleData().then(this.build);
+    this.getSampleData()
+      .then(this.buildPage)
+      .catch(Logger.catch);
   }
 
   getSampleData() {
-    return fetch('/assets/sample.json').then((data) => data.json());
+    return fetch(HomeComponent.DATA_URL)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(HomeComponent.FETCH_RESPONSE_ERROR);
+      })
+      .catch(Logger.catch);
   }
 
-  build(data) {
-    document.getElementById(this.appId).innerHTML = data.page;
+  buildPage(data) {
+    const element = document.getElementById(this.appId);
+    if (element) {
+      element.innerHTML = data.page;
+    }
     console.log(data.console); // eslint-disable-line no-console
   }
 }

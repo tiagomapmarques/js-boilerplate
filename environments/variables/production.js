87785a488f005c0c9b737120226ad96a99b4eabd
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2017-11-20 00:26:39 +0000

    Feature: Auto vendor modules (#28)

--- environments/variables/production.js
+++ environments/variables/production.js
@@ -1,6 +1,8 @@
 
 export const prodConfig = {
   ASSETS_URL: '/assets/',
+  // eslint-disable-next-line global-require
+  VERSION: require('../../package.json').version,
 };
 
 // eslint-disable-next-line no-default-export

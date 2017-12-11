b73e5aa0bb2edee1835c2d661fb64701446e16ee
Tiago Marques <tiago.map.marques@gmail.com>
2017-12-11 22:35:43 +0000

    Updated environment variables

--- environments/variables/development.js
+++ environments/variables/development.js
@@ -1,7 +1,10 @@
+import { page } from '../webpack/builders';
 import { packageJson } from './package-json';
 
 export const devEnvironment = {
+  ENVIRONMENT: 'development',
   VERSION: packageJson.version,
+  TITLE: page.title,
   SERVICES: {
     ASSETS: '/assets/',
   },

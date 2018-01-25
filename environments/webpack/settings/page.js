fa8dec75d585da5a5c49fa70d6f9efeffa649429
Tiago Marques <legendary.gates@gmail.com>
2018-01-25 00:38:58 +0000

    Added common meta tags

--- environments/webpack/settings/page.js
+++ environments/webpack/settings/page.js
@@ -1,13 +1,18 @@
+import { packageJson } from '../../package-json';
 import { paths } from './paths';
 
 export const pageConfig = {
   title: 'Js Boilerplate',
+  description: packageJson.description,
+  keywords: packageJson.keywords.reduce((collection, word) => `${collection},${word}`, '').substr(1),
+  author: packageJson.author,
+  copyright: 'https://raw.githubusercontent.com/tiagomapmarques/js-boilerplate/develop/LICENSE',
   template: '.index.ejs',
   cache: false,
 };
 
 const getPageConfig = minify => ({
-  title: pageConfig.title,
+  ...pageConfig,
   filename: `${paths.distAbsolute}/index.html`,
   template: `${paths.staticAbsolute}/${pageConfig.template}`,
   cache: pageConfig.cache,

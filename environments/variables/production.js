39a597d54257f11d9c8bbeb4a295f82e2aa3f9fc
Tiago Marques <legendary.gates@gmail.com>
2018-01-28 14:19:22 +0000

    Updated environment runtime variables

--- environments/variables/production.js
+++ environments/variables/production.js
@@ -4,6 +4,7 @@ import { pageConfig, packageJson } from '../webpack/settings';
 export const env = {
   ENVIRONMENT: environments.prod,
   VERSION: packageJson.version,
+  ROOTID: pageConfig.rootId,
   TITLE: pageConfig.title,
   SERVICES: {
     ASSETS: '/assets/',

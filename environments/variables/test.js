0f66a0e760f27e48c28841e471f7233082529908
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2018-01-28 17:24:06 +0000

    Feature: Default language (#68)

--- environments/variables/test.js
+++ environments/variables/test.js
@@ -5,6 +5,10 @@ export const env = {
   VERSION: project.version,
   ROOTID: 'mock-root-id',
   TITLE: 'Mock Title',
+  DEFAULTS: {
+    LANGUAGE: 'en_US',
+    LANGUAGE_SHORT: 'en',
+  },
   SERVICES: {
     ASSETS: '/mock-assets/',
   },

4761dd4c12905e9bf20d68bff0a8db89a8ecc100
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2018-05-03 14:19:30 +0100

    Feature: Webpack 4 (#73)

--- environments/webpack/dev.config.js
+++ environments/webpack/dev.config.js
@@ -3,7 +3,6 @@ import ExtendedDefinePlugin from 'extended-define-webpack-plugin';
 import PrerenderSpaPlugin from 'prerender-spa-plugin';
 
 import { environments } from '../';
-import { paths } from '../config';
 import { getVariables } from '../variables';
 import { app, favicon } from './settings';
 import { baseConfig } from './base.config';
@@ -14,6 +13,6 @@ export const config = {
     ...(baseConfig.plugins || []),
     new FaviconsWebpackPlugin(favicon.all),
     new ExtendedDefinePlugin(getVariables(environments.dev)),
-    new PrerenderSpaPlugin(paths.distAbsolute, app.routes),
+    new PrerenderSpaPlugin(app.rendering.staticDir, app.rendering.routes),
   ],
 };
--- environments/webpack/prod.config.js
+++ environments/webpack/prod.config.js
@@ -6,7 +6,6 @@ import ExtendedDefinePlugin from 'extended-define-webpack-plugin';
 import PrerenderSpaPlugin from 'prerender-spa-plugin';
 
 import { environments } from '../';
-import { paths } from '../config';
 import { getVariables } from '../variables';
 import { app, favicon, manifest, page, rules } from './settings';
 import { baseConfig } from './base.config';
@@ -14,15 +13,19 @@ import { baseConfig } from './base.config';
 export const config = {
   ...baseConfig,
   devtool: false,
+  mode: 'production',
   module: { rules: rules.minified },
+  optimization: {
+    ...baseConfig.optimization,
+    minimize: true,
+  },
   plugins: [
     ...(baseConfig.plugins || []),
     new HtmlWebpackPlugin(page.minified),
     new FaviconsWebpackPlugin(favicon.all),
     new ManifestJsonWebpackPlugin(manifest.minified),
     new ExtendedDefinePlugin(getVariables(environments.prod)),
-    new PrerenderSpaPlugin(paths.distAbsolute, app.routes),
-    new webpack.optimize.UglifyJsPlugin({ comments: false }),
+    new PrerenderSpaPlugin(app.rendering.staticDir, app.rendering.routes),
     new webpack.NoEmitOnErrorsPlugin(),
   ],
 };

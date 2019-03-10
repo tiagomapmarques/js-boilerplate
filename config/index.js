e9fae8b35086a6678c78260683dbf446daf7eb1f
Tiago Marques <tiagomapmarques@users.noreply.github.com>
2019-03-10 19:08:54 +0000

    Feature: Serve and watch (#134)

--- config/index.js
+++ config/index.js
@@ -1,9 +1,17 @@
 import { environments } from './environments';
 
-export const webpackInit = (requestedEnv) => {
+export const webpackInit = (requestedEnv, { serve, open, spa }) => {
   const actualEnv = (requestedEnv && environments[requestedEnv]) || environments.default;
   process.env.NODE_ENV = actualEnv;
 
   // eslint-disable-next-line global-require,import/no-dynamic-require
-  return require(`./${actualEnv}.config`).config;
+  const { config } = require(`./${actualEnv}.config`);
+
+  if (serve) {
+    // eslint-disable-next-line global-require
+    const { ServeAfterFirstBuildPlugin } = require('./serve-after-first-build-plugin');
+    config.plugins.push(new ServeAfterFirstBuildPlugin({ open, spa }));
+  }
+
+  return config;
 };

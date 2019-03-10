defedf2468ddb92b2741c9d1518ebfb75fbbb641
Tiago Marques <tiago.marques@virtual-identity.com>
2019-03-10 18:54:15 +0000

    Add custom serve plugin

--- config/index.js
+++ config/index.js
@@ -1,9 +1,16 @@
 import { environments } from './environments';
+import { ServeAfterFirstBuildPlugin } from './serve-after-first-build-plugin';
 
-export const webpackInit = (requestedEnv) => {
+export const webpackInit = (requestedEnv, { serve, open, spa }) => {
   const actualEnv = (requestedEnv && environments[requestedEnv]) || environments.default;
   process.env.NODE_ENV = actualEnv;
 
   // eslint-disable-next-line global-require,import/no-dynamic-require
-  return require(`./${actualEnv}.config`).config;
+  const { config } = require(`./${actualEnv}.config`);
+
+  if (serve) {
+    config.plugins.push(new ServeAfterFirstBuildPlugin({ open, spa }));
+  }
+
+  return config;
 };
---
+++ config/serve-after-first-build-plugin.js
@@ -0,0 +1,51 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import LocalWebServer from 'local-web-server';
+import reduceFlatten from 'reduce-flatten';
+import opn from 'opn';
+/* eslint-enable import/no-extraneous-dependencies */
+import os from 'os';
+
+const server = (port, open, spa, https): void => {
+  (new LocalWebServer()).listen({
+    port,
+    https,
+    compress: true,
+    directory: 'public',
+    spa: spa ? 'index.html' : undefined,
+  });
+
+  const ipList = Object.keys(os.networkInterfaces())
+    .map(key => os.networkInterfaces()[key])
+    .reduce(reduceFlatten, [])
+    .filter(networkInterface => networkInterface.family === 'IPv4')
+    .map(networkInterface => networkInterface.address);
+
+  ipList.unshift(os.hostname());
+  const urls = ipList.map(address => `http${https ? 's' : ''}://${address}:${port}`);
+
+  // eslint-disable-next-line no-console
+  console.log(`\n\nServing at ${urls.join(', ')}\n\n`);
+
+  if (open) {
+    opn(urls[1]);
+  }
+};
+
+export class ServeAfterFirstBuildPlugin {
+  constructor(options) {
+    this.isFirstCompilation = true;
+    this.port = options.port || 8000;
+    this.open = options.open || false;
+    this.spa = options.spa || false;
+    this.secure = options.secure || false;
+  }
+
+  apply(compiler) {
+    compiler.hooks.afterEmit.tap('ServeAfterFirstBuild', () => {
+      if (this.isFirstCompilation) {
+        this.isFirstCompilation = false;
+        server(this.port, this.open, this.spa, this.secure);
+      }
+    });
+  }
+}

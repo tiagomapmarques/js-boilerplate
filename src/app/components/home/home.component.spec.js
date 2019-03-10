c8a817a96977f0fca5dcd5bef264f6dc0c1dbc78
Tiago Marques <tiago.marques@virtual-identity.com>
2019-03-10 16:34:18 +0000

    Simplify tests

--- src/app/components/home/home.component.spec.js
+++ src/app/components/home/home.component.spec.js
@@ -1,6 +1,6 @@
 import { HelperService } from 'services';
 
-import { HomeComponent } from './home.component';
+import { HomeComponent } from '.';
 import style from './home.style';
 
 jest.mock('services', () => ({

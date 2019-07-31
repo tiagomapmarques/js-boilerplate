762e7eb2a9a7e7b69e4f5d8926b9ee0366d3ba7d
Tiago Marques <legendary.gates@gmail.com>
2019-07-31 21:03:02 +0100

    Add runtime color variable parsing

--- config/settings/javascript-to-sass.js
+++ config/settings/javascript-to-sass.js
@@ -1,10 +1,21 @@
 
+const isNumber = (variable) => {
+  const value = parseInt(variable, 10);
+  // Comparing against itself to check if it is NaN
+  // eslint-disable-next-line no-self-compare
+  return value === value && typeof value === 'number';
+};
+
+const toSassVariable = variable => (
+  !isNumber(variable) && variable[0] !== '#' ? `'${variable}'` : variable
+);
+
 const flattenObject = (variables, prefix = '') => Object.keys(variables)
   .reduce((accumulator, key) => ({
     ...accumulator,
     ...(typeof variables[key] === 'object'
       ? flattenObject(variables[key], `${prefix}${prefix ? '_' : ''}${key}`)
-      : { [`${prefix}${prefix ? '_' : ''}${key}`]: variables[key] }
+      : { [`${prefix}${prefix ? '_' : ''}${key}`]: toSassVariable(variables[key]) }
     ),
   }), {});
 
@@ -12,5 +23,5 @@ export const javascriptToSass = (variables) => {
   const flattenVariables = flattenObject(variables);
 
   return Object.keys(flattenVariables)
-    .reduce((accumulator, key) => `${accumulator}$${key}:'${flattenVariables[key]}';`, '');
+    .reduce((accumulator, key) => `${accumulator}$${key}:${flattenVariables[key]};`, '');
 };

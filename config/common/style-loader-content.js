e1eb5970cc6960a82d656355154cabc707dec299
Tiago Marques <legendary.gates@gmail.com>
2019-08-15 19:28:56 +0100

    Convert style loader to es5

--- config/common/style-loader-content.js
+++ config/common/style-loader-content.js
@@ -1,21 +1,18 @@
 
-const loaderDocument = global.document;
-// eslint-disable-next-line import/no-dynamic-require,no-undef
-let style = require(__STYLE__);
+var loaderDocument = global.document;
+var style = require(__STYLE__);
 
 if (typeof style === 'string') {
   style = [[module.id, style, '']];
 }
 
-const content = (style && style[0] && style[0][1]) || '';
-// NOTE: allow calling hasOwnProperty due to browser compatibilities
-// eslint-disable-next-line no-prototype-builtins
-const cssInJavacript = style.hasOwnProperty('toString');
-const isCustomElement = style.toString().indexOf(':host') >= 0;
-const autoApplyStyle = cssInJavacript && !isCustomElement;
+var content = (style && style[0] && style[0][1]) || '';
+var cssInJavacript = style.hasOwnProperty('toString');
+var isCustomElement = style.toString().indexOf(':host') >= 0;
+var autoApplyStyle = cssInJavacript && !isCustomElement;
 
 if (autoApplyStyle) {
-  const element = loaderDocument.createElement('style');
+  var element = loaderDocument.createElement('style');
   element.innerHTML = content;
   loaderDocument.head.appendChild(element);
 }

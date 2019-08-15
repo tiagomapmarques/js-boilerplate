2cdfa6dc0d11855ad2d0ff38c884fad59e2997f8
Tiago Marques <legendary.gates@gmail.com>
2019-08-15 19:29:19 +0100

    Add assign method

--- config/common/style-loader-content.js
+++ config/common/style-loader-content.js
@@ -1,3 +1,15 @@
+/* eslint-disable */
+var __assign = (this && this.__assign) || function () {
+  __assign = Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+      for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+        t[p] = s[p];
+    }
+    return t;
+  };
+  return __assign.apply(this, arguments);
+};
 
 var loaderDocument = global.document;
 var style = require(__STYLE__);
@@ -17,7 +29,11 @@ if (autoApplyStyle) {
   loaderDocument.head.appendChild(element);
 }
 
-module.exports = {
-  ...(style.locals || style),
-  toString: () => (!autoApplyStyle ? content : ''),
-};
+module.exports = __assign(
+  {},
+  (style.locals || style),
+  {
+    toString: function () { return (!autoApplyStyle ? content : ''); }
+  }
+);
+/* eslint-enable */

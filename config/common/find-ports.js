c59cfbb805059c171d5cbfa837c4f8c7c60eecd3
Tiago Marques <legendary.gates@gmail.com>
2019-10-03 22:54:41 +0100

    Transpile code to node-compatible

--- config/common/find-ports.js
+++ config/common/find-ports.js
@@ -1,12 +1,17 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import getPort from 'get-port';
+const getPort = require('get-port');
 // eslint-disable-next-line import/no-extraneous-dependencies
-import synchronizedPromise from 'synchronized-promise';
+const synchronizedPromise = require('synchronized-promise');
 
 const getPortSync = synchronizedPromise(getPort);
 
-export const findPort = (port, range = 999) => getPortSync({
+const findPort = (port, range = 999) => getPortSync({
   port: getPort.makeRange(port, port + range),
 });
 
-export const findPorts = (ports, range) => ports.map((port) => findPort(port, range));
+const findPorts = (ports, range) => ports.map((port) => findPort(port, range));
+
+module.exports = {
+  findPort,
+  findPorts,
+};
